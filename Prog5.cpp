05a84a1b6ce1c47e517ff6851fea948022a5a67a
vladkk345 <55635774+vladkk345@users.noreply.github.com>
2021-05-30 18:09:44 +0300

    Add files via upload

---
+++ Prog5.cpp
@@ -0,0 +1,50 @@
+﻿// Prog5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <Windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int row;
+    char ch;
+    char bukva;
+   
+    printf("Введите любую букву:\n");
+    scanf_s("%c", &bukva);
+    char ROWS = bukva - '@';
+
+    for (row = 0; row < ROWS; row++)
+    {
+        char letter = 'A';
+        for (char i = 0; i < ROWS - row; i++)
+        {
+            printf("%c",' ');
+        }
+        for (ch = 'A'; ch <= 'A' + row; ch++)
+        {
+            printf("%c", ch);
+           
+        }
+        if (row>0)
+        {
+            letter += row - 1;
+            for (ch = letter; ch > letter - row; ch--)
+            {
+                printf("%c", ch);
+
+            }
+        }
+        
+        printf("\n");
+
+        
+    }
+    
+    return 0;
+}
+
